de0aa8aacf42a6875d0636efd0bd2ccc8cb476a3
Andras Biro <bbandi86@gmail.com>
2014-03-13 11:52:20 +0100

    convert time, better handling of filenames, automaticly opens last file if nothing is specified

--- apps/szte/InterferenceMeasure/plot.py
+++ apps/szte/InterferenceMeasure/plot.py
@@ -2,7 +2,20 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 import sys
-f = open(sys.argv[1], 'r')
+import os
+if len(sys.argv) >= 2:
+	filename = sys.argv[1]
+else:
+	maxtime = 0;
+	for f in os.listdir('.'):
+		if os.path.isfile(f) and os.path.basename(f).endswith(".txt"):
+			if os.path.getmtime(f) > maxtime:
+				maxtime = os.path.getmtime(f)
+				filename = os.path.basename(f)
+	
+LENGTHFREQ=62500
+(name, sep, length) = filename.rpartition('.')[0].rpartition('_')
+f = open(filename, 'r')
 xlist = []
 ylist = []
 zlist = []
@@ -10,9 +23,15 @@ timelist = []
 counter = 0;
 for line in f:
   ylist.append(line)
-  timelist.append(counter)
   counter+=1
-
+lengthus=1000000*int(length)/LENGTHFREQ
+timeusbase=lengthus/counter
+#print(counter)
+#print(length)
+#print(lengthus)
+#print(timeusbase)
+for i in range(0, counter):
+	timelist.append(i*timeusbase)
 plt.plot(timelist, ylist)
 #fig, ax = plt.subplots()
 #ax.plot(timelist, xlist, label="X")
@@ -24,8 +43,7 @@ plt.plot(timelist, ylist)
 #plt.rc('font', **font)
 #legend = ax.legend(prop={'size':30})
 #legend = ax.legend()
-name = sys.argv[1].split('.')
-plt.title(name[0])
-plt.xlabel('time')
+plt.title(name)
+plt.xlabel('time [us]')
 plt.ylabel('RSSI')
 plt.show()
