ec84440f7774470c4c8867ba7349b64aacdf66cc
Andras Biro <bbandi86@gmail.com>
2014-09-15 16:30:39 +0200

    modified platform for bootloader exit

--- tos/platforms/ucprotona/platform.h
+++ tos/platforms/ucprotona/platform.h
@@ -1,3 +1,8 @@
 // disable watchdog timer at startup (see AVR132: Using the Enhanced Watchdog Timer)
 #include <avr/wdt.h> 
-#define platform_bootstrap() { MCUSR = 0; wdt_disable(); }
+#if defined(BOOTLOADER_MAGIC_PTR) 
+uint16_t *magic=(uint16_t*)BOOTLOADER_MAGIC_PTR;
+#define platform_bootstrap() { if( *magic  == BOOTLOADER_MAGIC_VALUE) {  asm("jmp 0000");} MCUSR = 0; wdt_disable(); }
+#else
+#define platform_bootstrap() {MCUSR = 0; wdt_disable(); }
+#endif
