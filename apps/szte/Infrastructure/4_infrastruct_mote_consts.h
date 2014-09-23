fd8b3ba0fa99fc246957188221fdeeb9f8d1bf00
Gyoorey <gyooorey@hotmail.com>
2014-09-23 12:53:27 +0200

    Includes that define constatnts and schedule scenes for the scheduler.

---
+++ apps/szte/Infrastructure/4_infrastruct_mote_consts.h
@@ -0,0 +1,3 @@
+#define NUMBER_OF_SLOTS 20
+#define NUMBER_OF_RX 6
+#define schedule_include "4_infrastruct_mote_schedule.h"
---
+++ apps/szte/Infrastructure/4_infrastruct_mote_debug_consts.h
@@ -0,0 +1,3 @@
+#define NUMBER_OF_SLOTS 52
+#define NUMBER_OF_RX 6
+#define schedule_include "4_infrastruct_mote_debug_schedule.h"
