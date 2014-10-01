6a28f769b52321ab7f98a2bb38ddf4b7f35b5318
Andras Biro <bbandi86@gmail.com>
2014-10-01 11:20:16 +0200

    only tx frames before sync send (to let the motes calculate the results)

--- apps/szte/Infrastructure/4_infrastruct_mote_consts.h
+++
@@ -1,3 +0,0 @@
-#define NUMBER_OF_SLOTS 20
-#define NUMBER_OF_RX 6
-#define schedule_include "4_infrastruct_mote_schedule.h"
--- apps/szte/Infrastructure/4_infrastruct_mote_debug_consts.h
+++
@@ -1,3 +0,0 @@
-#define NUMBER_OF_SLOTS 52
-#define NUMBER_OF_RX 6
-#define schedule_include "4_infrastruct_mote_debug_schedule.h"
