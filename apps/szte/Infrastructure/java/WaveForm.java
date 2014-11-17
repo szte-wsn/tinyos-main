6f017feb5f680cdd54359ddcba60edd749d8a9bf
Andras Biro <bbandi86@gmail.com>
2014-11-17 16:29:27 +0100

    added pre-generated files

--- apps/szte/Infrastructure/java/WaveForm.java
+++ apps/szte/Infrastructure/java/WaveForm.java
@@ -7,12 +7,12 @@
 public class WaveForm extends net.tinyos.message.Message {
 
     /** The default size of this message type in bytes. */
-    public static final int DEFAULT_MESSAGE_SIZE = 82;
+    public static final int DEFAULT_MESSAGE_SIZE = 98;
 
     /** The Active Message type associated with this message. */
     public static final int AM_TYPE = 7;
 
-    /** Create a new WaveForm of size 82. */
+    /** Create a new WaveForm of size 98. */
     public WaveForm() {
         super(DEFAULT_MESSAGE_SIZE);
         amTypeSet(AM_TYPE);
@@ -92,7 +92,7 @@ public class WaveForm extends net.tinyos.message.Message {
       } catch (ArrayIndexOutOfBoundsException aioobe) { /* Skip field */ }
       try {
         s += "  [data=";
-        for (int i = 0; i < 80; i++) {
+        for (int i = 0; i < 96; i++) {
           s += "0x"+Long.toHexString(getElement_data(i) & 0xff)+" ";
         }
         s += "]\n";
@@ -254,7 +254,7 @@ public class WaveForm extends net.tinyos.message.Message {
      */
     public static int offset_data(int index1) {
         int offset = 16;
-        if (index1 < 0 || index1 >= 80) throw new ArrayIndexOutOfBoundsException();
+        if (index1 < 0 || index1 >= 96) throw new ArrayIndexOutOfBoundsException();
         offset += 0 + index1 * 8;
         return (offset / 8);
     }
@@ -264,7 +264,7 @@ public class WaveForm extends net.tinyos.message.Message {
      */
     public static int offsetBits_data(int index1) {
         int offset = 16;
-        if (index1 < 0 || index1 >= 80) throw new ArrayIndexOutOfBoundsException();
+        if (index1 < 0 || index1 >= 96) throw new ArrayIndexOutOfBoundsException();
         offset += 0 + index1 * 8;
         return offset;
     }
@@ -273,7 +273,7 @@ public class WaveForm extends net.tinyos.message.Message {
      * Return the entire array 'data' as a short[]
      */
     public short[] get_data() {
-        short[] tmp = new short[80];
+        short[] tmp = new short[96];
         for (int index0 = 0; index0 < numElements_data(0); index0++) {
             tmp[index0] = getElement_data(index0);
         }
@@ -307,14 +307,14 @@ public class WaveForm extends net.tinyos.message.Message {
      * Return the total size, in bytes, of the array 'data'
      */
     public static int totalSize_data() {
-        return (640 / 8);
+        return (768 / 8);
     }
 
     /**
      * Return the total size, in bits, of the array 'data'
      */
     public static int totalSizeBits_data() {
-        return 640;
+        return 768;
     }
 
     /**
@@ -342,7 +342,7 @@ public class WaveForm extends net.tinyos.message.Message {
      * Return the number of elements in the array 'data'
      */
     public static int numElements_data() {
-        return 80;
+        return 96;
     }
 
     /**
@@ -350,7 +350,7 @@ public class WaveForm extends net.tinyos.message.Message {
      * for the given dimension.
      */
     public static int numElements_data(int dimension) {
-      int array_dims[] = { 80,  };
+      int array_dims[] = { 96,  };
         if (dimension < 0 || dimension >= 1) throw new ArrayIndexOutOfBoundsException();
         if (array_dims[dimension] == 0) throw new IllegalArgumentException("Array dimension "+dimension+" has unknown size");
         return array_dims[dimension];
@@ -372,7 +372,7 @@ public class WaveForm extends net.tinyos.message.Message {
      * Read the array 'data' as a String
      */
     public String getString_data() { 
-         char carr[] = new char[Math.min(net.tinyos.message.Message.MAX_CONVERTED_STRING_LENGTH,80)];
+         char carr[] = new char[Math.min(net.tinyos.message.Message.MAX_CONVERTED_STRING_LENGTH,96)];
          int i;
          for (i = 0; i < carr.length; i++) {
              if ((char)getElement_data(i) == (char)0) break;
