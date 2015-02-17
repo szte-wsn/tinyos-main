e818a02189f9cc52d3486ffb3bdd9df0733b9c70
Gyoorey <gyooorey@hotmail.com>
2015-02-17 16:28:27 +0100

    New schedule: PHASEMAP_TEST_8

--- apps/szte/Infrastructure/InfrastructureSettings.h
+++ apps/szte/Infrastructure/InfrastructureSettings.h
@@ -30,6 +30,7 @@ enum {
 #define SIX_MOTE 12
 #define LOC_TEST_9_GRID 13
 #define LOC_4TX_4ANCHORRX 14
+#define PHASEMAP_TEST_8 15
 
 #ifndef MEASURE_TYPE
 #error "Please define MEASURE_TYPE"
@@ -147,6 +148,37 @@ enum {
 		{ RSYN,   RX,  W10, RSYN, RSYN, RSYN, RSYN, RSYN, RSYN, RSYN, SSYN, RSYN},
 		{ RSYN,   RX,  W10, RSYN, RSYN, RSYN, RSYN, RSYN, RSYN, RSYN, RSYN, SSYN}
 	};
+#elif MEASURE_TYPE == PHASEMAP_TEST_8
+	#define NUMBER_OF_INFRAST_NODES 8
+	#define NUMBER_OF_SLOTS 9
+	#define NUMBER_OF_RX 2
+	const_uint8_t motesettings[NUMBER_OF_INFRAST_NODES][NUMBER_OF_SLOTS] = {
+		//   0     1     2     3     4     5
+		{ RSYN,  TX1,  TX1,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+		{ RSYN,  TX2,  TX2,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+		{ SSYN,   RX,   RX,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+		{ RSYN,   RX,   RX,  W10, SSYN, RSYN, RSYN, RSYN, RSYN},
+		{ RSYN,   RX,   RX,  W10, RSYN, SSYN, RSYN, RSYN, RSYN},
+		{ RSYN,   RX,   RX,  W10, RSYN, RSYN, SSYN, RSYN, RSYN},
+		{ RSYN,   RX,   RX,  W10, RSYN, RSYN, RSYN, SSYN, RSYN},
+		{ RSYN,   RX,   RX,  W10, RSYN, RSYN, RSYN, RSYN, SSYN}
+	};
+//elfelejtettem melyik kell, amiben 2 vagy amiben 3 meres van	
+//#elif MEASURE_TYPE == PHASEMAP_TEST_8
+//	#define NUMBER_OF_INFRAST_NODES 8
+//	#define NUMBER_OF_SLOTS 10
+//	#define NUMBER_OF_RX 3
+//	const_uint8_t motesettings[NUMBER_OF_INFRAST_NODES][NUMBER_OF_SLOTS] = {
+//		//   0     1     2     3     4     5
+//		{ RSYN,  TX1,  TX1,  TX1,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+//		{ RSYN,  TX2,  TX2,  TX2,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+//		{ SSYN,   RX,   RX,   RX,  W10, RSYN, RSYN, RSYN, RSYN, RSYN},
+//		{ RSYN,   RX,   RX,   RX,  W10, SSYN, RSYN, RSYN, RSYN, RSYN},
+//		{ RSYN,   RX,   RX,   RX,  W10, RSYN, SSYN, RSYN, RSYN, RSYN},
+//		{ RSYN,   RX,   RX,   RX,  W10, RSYN, RSYN, SSYN, RSYN, RSYN},
+//		{ RSYN,   RX,   RX,   RX,  W10, RSYN, RSYN, RSYN, SSYN, RSYN},
+//		{ RSYN,   RX,   RX,   RX,  W10, RSYN, RSYN, RSYN, RSYN, SSYN}
+//	};
 #elif MEASURE_TYPE == PHASEMAP_TEST_6
 	#define NUMBER_OF_INFRAST_NODES 6
 	#define NUMBER_OF_SLOTS 6
