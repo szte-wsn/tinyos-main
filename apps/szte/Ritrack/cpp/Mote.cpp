67690983b2795e208154730925a4280869bcf99e
Miklos Maroti <mmaroti@gmail.com>
2015-04-14 00:56:29 +0200

    fixed mote.cpp

--- apps/szte/Ritrack/cpp/Mote.cpp
+++ apps/szte/Ritrack/cpp/Mote.cpp
@@ -5,17 +5,17 @@ Mote::Mote(short ID_in, Position<double>& pos_in): ID(ID_in), pos(pos_in){
 
 Mote::Mote(const short ID_in, const double x, const double y): ID(ID_in), pos(x,y){
 }
-	
+
 const short Mote::getID() const { return Mote::ID; }
-	
-	
+
+
 const Position<double>& Mote::getPosition() const { return Mote::pos; }
 
-void Mote::setPosition(const Position<double>& pos_in){ 
+void Mote::setPosition(const Position<double>& pos_in){
 	Mote::pos.setX(pos_in.getX());
 	Mote::pos.setY(pos_in.getY());
 }
-void Mote::setPosition(const double& x, const double& y){	
+void Mote::setPosition(const double& x, const double& y){
 	Mote::pos.setX(x);
 	Mote::pos.setY(y);
 }
@@ -26,6 +26,7 @@ void Mote::setID(short id){
 
 std::ostream& operator<<(std::ostream& os, const Mote& mote){
 	os << std::setw(2) << mote.ID << ": " << mote.pos;
+	return os;
 }
 
 Mote& Mote::operator=(Mote& other){
